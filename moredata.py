c935007bfdcafd52f89eabe174448dae502435f8
Nino <nino.basic@famnit.upr.si>
2017-05-31 17:45:31 +0200

    We can download pictures now!

--- moredata.py
+++ moredata.py
@@ -1,9 +1,17 @@
 import requests
 import re
 
-def more_info(s):
+def get_picture(s):
     url = 'https://www.famnit.upr.si' + s
     r = requests.get(url)
-    print(r.text)
+    img_pattern = r'http://www\.famnit\.upr\.si/sl/resources/images/o-fakulteti/osebje/.*\.jpg'
+    m = re.search(img_pattern, r.text)
+    url_picture = m.group(0)
+    r = requests.get(url_picture)
+    with open('pictures/ado.jpg', 'wb') as fd:
+        for chunk in r.iter_content(chunk_size=128):
+            fd.write(chunk)
+    
 
-print(more_info('/en/about-faculty/staff/andy'))
+print(get_picture('/en/about-faculty/staff/ademir.hujdurovic/'))
+
