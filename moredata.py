7794cdc16b20782b1b0e90a8b2d08112504c7636
Nino <nino.basic@famnit.upr.si>
2017-05-31 18:42:42 +0200

    Now we can download all pictures.

--- moredata.py
+++ moredata.py
@@ -1,17 +1,28 @@
 import requests
 import re
 
+# git reset --hard <funny string>
+# git pull origin master
+
 def get_picture(s):
     url = 'https://www.famnit.upr.si' + s
     r = requests.get(url)
-    img_pattern = r'http://www\.famnit\.upr\.si/sl/resources/images/o-fakulteti/osebje/.*\.jpg'
+    allowed_extensions = ['jpg', 'png']
+    img_pattern = r'http://www\.famnit\.upr\.si/sl/resources/images/o-fakulteti/osebje/(.*\.({0}))'.format(
+        '|'.join(allowed_extensions)
+    )
     m = re.search(img_pattern, r.text)
     url_picture = m.group(0)
+    picture_name = m.group(1)
     r = requests.get(url_picture)
-    with open('pictures/ado.jpg', 'wb') as fd:
+    with open('pictures/{0}'.format(picture_name), 'wb') as fd:
         for chunk in r.iter_content(chunk_size=128):
             fd.write(chunk)
-    
 
-print(get_picture('/en/about-faculty/staff/ademir.hujdurovic/'))
+with open('staff.csv') as f:
+    for person in f:
+        info = person.split(';')
+        url = info[3]
+        print('Downloading {0} {1} ...'.format(info[0], info[1]))
+        get_picture(url)
 
